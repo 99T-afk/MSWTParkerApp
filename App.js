d7b5c03ac32cdf8cccb705907a490a51fdd79954
99T-afk <60881328+99T-afk@users.noreply.github.com>
2021-10-26 22:45:51 +0100

    Added icons to homepage

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+/**
+ * IMPORTANT COMMANDS:
+ * npm install    <-- gets all node_modules
+ * git pull       <-- gets latest on github
+ * git push       <-- pushs changes (REMEMBER TO COMMIT FIRST)
+ * 
+ */
+
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,26 +14,54 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import FavouriteScreen from './screens/FavouriteScreen';
+import { AntDesign } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Favourites" component={FavouriteScreen} />
+      <Tab.Screen
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={24} color="black" />
+          ),
+          headerShown: false,
+            }}
+        name="Home" 
+        component={HomeScreen} />     
+      <Tab.Screen
+        options={{
+        tabBarLabel: 'Favourites',
+        tabBarIcon: ({ color, size }) => (
+          <AntDesign name="staro" size={24} color="black" />
+          ),
+          }}
+        name="Favourites"
+        component={FavouriteScreen} />
     </Tab.Navigator>
   );
 }
 
 
 export default function App() {
-  return (
+  return (  
     <NavigationContainer>
       <MyTabs />
     </NavigationContainer>
   );
 }
+
+/*
+      <Image
+        style={styles.imageLogo}
+        source={{
+          uri: 'http://images.magicseaweed.com/news/311.jpg',
+        }}
+      />
+
+*/
 
 const styles = StyleSheet.create({
   container: {
@@ -33,4 +70,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageLogo: {
+    width: 150,
+    height: 40,
+    alignItems: 'flex-start',
+  }
 });
