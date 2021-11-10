aa29bcb2fcd8fe2d91e7d46db74963e3be50517e
99T-afk <60881328+99T-afk@users.noreply.github.com>
2021-11-10 22:15:52 +0000

    locations update

--- components/Places.js
+++ components/Places.js
@@ -11,9 +11,9 @@ const Locations = [
     {"id" : 7, "name" : "Brighton", "distance" : "66 mi.", "weatherIcon" : "day-rain", "temp": "9","lat": 50.827778 , "long": -0.15338},
     {"id" : 8, "name" : "Dartmouth", "distance" : "70 mi.", "weatherIcon" : "day-sunny", "temp": "10","lat": 50.352517 , "long": -0.152778},
     {"id" : 9, "name" : "Kimmeridge Bay", "distance" : "72 mi.", "weatherIcon" : "day-sunny", "temp": "11","lat": 50.5890309772, "long": -2.0584330996},
-    {"id" : 10, "name" : "Torquay", "distance" : "121 mi.", "weatherIcon" : "day-sunny", "temp": "8","lat": 50.461921 , "long": -3.525315},
-    {"id" : 11, "name" : "Manchester", "distance" : "176 mi.", "weatherIcon" : "lightning", "temp": "7","lat":53.483959 , "long": -2.244644},
-    {"id" : 12, "name" : "Inverness", "distance" : "394 mi.", "weatherIcon" : "rains", "temp": "6","lat": 57.477772, "long":  -4.224721},
+    {"id" : 10, "name" : "Torquay", "distance" : "73 mi.", "weatherIcon" : "day-sunny", "temp": "8","lat": 50.461921 , "long": -3.525315},
+    {"id" : 11, "name" : "Manchester", "distance" : "189 mi.", "weatherIcon" : "lightning", "temp": "7","lat":53.483959 , "long": -2.244644},
+    {"id" : 12, "name" : "Inverness", "distance" : "479 mi.", "weatherIcon" : "rains", "temp": "6","lat": 57.477772, "long":  -4.224721},
 ];
 
 const getLocations = () => {
